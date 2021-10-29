585e4fc6b347b5961cd1254971d33ff0fa39e626
k.koide <k.koide@aist.go.jp>
2021-10-29 19:47:31 +0900

    vision support

--- src/glim_ros.cpp
+++ src/glim_ros.cpp
@@ -1,3 +1,4 @@
+#include <glob.h>
 #include <memory>
 #include <iostream>
 #include <boost/format.hpp>
@@ -7,7 +8,10 @@
 #include <ros/package.h>
 
 #include <sensor_msgs/Imu.h>
+#include <sensor_msgs/Image.h>
+#include <sensor_msgs/CompressedImage.h>
 #include <sensor_msgs/PointCloud2.h>
+#include <cv_bridge/cv_bridge.h>
 #include <nmea_msgs/Sentence.h>
 #include <rosgraph_msgs/Clock.h>
 
@@ -21,9 +25,11 @@
 #include <glim/backend/async_sub_mapping.hpp>
 #include <glim/backend/sub_mapping.hpp>
 #include <glim/backend/async_global_mapping.hpp>
-#include <glim/backend/global_mapping_ct.hpp>
 #include <glim/backend/global_mapping.hpp>
 #include <glim/viewer/standard_viewer.hpp>
+
+#include <glim_ext/dbow_loop_detector.hpp>
+#include <glim_ext/scan_context_loop_detector.hpp>
 
 #include <glim/util/easy_profiler.hpp>
 #include <glim_ros/rviz_viewer.hpp>
@@ -37,8 +43,10 @@ public:
     rviz_viewer.reset(new glim::RvizViewer);
     standard_viewer.reset(new glim::StandardViewer);
 
+    dbow_loop_detector.reset(new glim::DBoWLoopDetector);
+    // sc_loop_detector.reset(new glim::ScanContextLoopDetector);
+
     preprocessor.reset(new glim::CloudPreprocessor);
-    // std::shared_ptr<glim::OdometryEstimationBase> odom(new glim::OdometryEstimationCT);
     std::shared_ptr<glim::OdometryEstimationBase> odom(new glim::OdometryEstimation);
     odometry_estimation.reset(new glim::AsyncOdometryEstimation(odom));
 
@@ -47,6 +55,12 @@ public:
 
     std::shared_ptr<glim::GlobalMappingBase> global(new glim::GlobalMapping);
     global_mapping.reset(new glim::AsyncGlobalMapping(global));
+  }
+
+  void insert_image(const double stamp, const cv::Mat& image) {
+    odometry_estimation->insert_image(stamp, image);
+    sub_mapping->insert_image(stamp, image);
+    global_mapping->insert_image(stamp, image);
   }
 
   void insert_imu(const double stamp, const Eigen::Vector3d& linear_acc, const Eigen::Vector3d& angular_vel) {
@@ -92,6 +106,9 @@ private:
   std::unique_ptr<glim::AsyncSubMapping> sub_mapping;
   std::unique_ptr<glim::AsyncGlobalMapping> global_mapping;
 
+  std::unique_ptr<glim::DBoWLoopDetector> dbow_loop_detector;
+  std::unique_ptr<glim::ScanContextLoopDetector> sc_loop_detector;
+
   std::unique_ptr<glim::RvizViewer> rviz_viewer;
   std::unique_ptr<glim::StandardViewer> standard_viewer;
 };
@@ -103,40 +120,75 @@ int main(int argc, char** argv) {
 
   GlimROS glim_ros;
 
-  const std::string bag_filename = "/home/koide/datasets/lio_sam/rooftop_ouster_dataset.bag";
-  std::vector<std::string> topics = {"/points_raw", "/imu_raw"};
+  const auto read_bag = [&](const std::string& bag_filename, const std::vector<std::string>& topics) {
+    std::cout << "opening " << bag_filename << std::endl;
 
-  rosbag::Bag bag(bag_filename, rosbag::bagmode::Read);
-  if (!bag.isOpen()) {
-    std::cerr << glim::console::bold_red << "error: failed to open " << bag_filename << glim::console::reset << std::endl;
-    return 1;
+    rosbag::Bag bag(bag_filename, rosbag::bagmode::Read);
+    if(!bag.isOpen()) {
+      std::cerr << glim::console::bold_red << "error: failed to open " << bag_filename << glim::console::reset << std::endl;
+      return false;
+    }
+
+    for(rosbag::MessageInstance const m : rosbag::View(bag, rosbag::TopicQuery(topics))) {
+      if(!glim_ros.spin_once()) {
+        return false;
+      }
+
+      const auto imu_msg = m.instantiate<sensor_msgs::Imu>();
+      if(imu_msg) {
+        const double stamp = imu_msg->header.stamp.toSec();
+        const auto& linear_acc = imu_msg->linear_acceleration;
+        const auto& angular_vel = imu_msg->angular_velocity;
+        glim_ros.insert_imu(stamp, Eigen::Vector3d(linear_acc.x, linear_acc.y, linear_acc.z), Eigen::Vector3d(angular_vel.x, angular_vel.y, angular_vel.z));
+      }
+
+      const auto points_msg = m.instantiate<sensor_msgs::PointCloud2>();
+      if(points_msg) {
+        auto raw_points = glim::RawPoints::extract(points_msg);
+        glim_ros.insert_frame(raw_points);
+      }
+
+      const auto compressed_img_msg = m.instantiate<sensor_msgs::CompressedImage>();
+      if(compressed_img_msg) {
+        auto cv_image = cv_bridge::toCvCopy(compressed_img_msg, "bgr8");
+        glim_ros.insert_image(compressed_img_msg->header.stamp.toSec(), cv_image->image);
+      }
+
+      // Ros-related
+      rosgraph_msgs::Clock::Ptr clock_msg(new rosgraph_msgs::Clock);
+      clock_msg->clock.sec = m.getTime().sec;
+      clock_msg->clock.nsec = m.getTime().nsec;
+      clock_pub.publish(clock_msg);
+      ros::spinOnce();
+    }
+
+    return true;
+  };
+
+  // const std::string bag_filename = "/home/koide/datasets/lio_sam/rooftop_ouster_dataset.bag";
+  // std::vector<std::string> topics = {"/points_raw", "/imu_raw"};
+
+  // const std::string bag_filename = "/home/koide/datasets/map_iv/data2/points.bag";
+  // std::vector<std::string> topics = {"/points_raw_ex"};
+
+  const std::string bag_path = "/home/koide/datasets/newer_college/01_short_experiment/rosbag_compressed/*";
+  std::vector<std::string> topics = {"/os1_cloud_node/imu", "/os1_cloud_node/points", "/camera/infra1/image_rect_raw/compressed"};
+
+  std::vector<std::string> bag_filenames;
+
+  glob_t globbuf;
+  int ret = glob(bag_path.c_str(), 0, nullptr, &globbuf);
+  for(int i = 0; i < globbuf.gl_pathc; i++) {
+    bag_filenames.push_back(globbuf.gl_pathv[i]);
   }
+  globfree(&globbuf);
 
-  for (rosbag::MessageInstance const m : rosbag::View(bag, rosbag::TopicQuery(topics))) {
-    if (!glim_ros.spin_once()) {
+  std::sort(bag_filenames.begin(), bag_filenames.end());
+
+  for(const auto& bag_filename : bag_filenames) {
+    if(!read_bag(bag_filename, topics)) {
       break;
     }
-
-    const auto imu_msg = m.instantiate<sensor_msgs::Imu>();
-    if (imu_msg) {
-      const double stamp = imu_msg->header.stamp.toSec();
-      const auto& linear_acc = imu_msg->linear_acceleration;
-      const auto& angular_vel = imu_msg->angular_velocity;
-      glim_ros.insert_imu(stamp, Eigen::Vector3d(linear_acc.x, linear_acc.y, linear_acc.z), Eigen::Vector3d(angular_vel.x, angular_vel.y, angular_vel.z));
-    }
-
-    const auto points_msg = m.instantiate<sensor_msgs::PointCloud2>();
-    if (points_msg) {
-      auto raw_points = glim::RawPoints::extract(points_msg);
-      glim_ros.insert_frame(raw_points);
-    }
-
-    // Ros-related
-    rosgraph_msgs::Clock::Ptr clock_msg(new rosgraph_msgs::Clock);
-    clock_msg->clock.sec = m.getTime().sec;
-    clock_msg->clock.nsec = m.getTime().nsec;
-    clock_pub.publish(clock_msg);
-    ros::spinOnce();
   }
 
   std::this_thread::sleep_for(std::chrono::seconds(1));
