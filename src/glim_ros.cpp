4ed11090a75fe72e6b110418ad0c6e31e4903925
k.koide <k.koide@aist.go.jp>
2021-11-01 23:48:32 +0900

    hilti

--- src/glim_ros.cpp
+++ src/glim_ros.cpp
@@ -28,8 +28,10 @@
 #include <glim/backend/global_mapping.hpp>
 #include <glim/viewer/standard_viewer.hpp>
 
+#include <glim_ext/util/config_ext.hpp>
 #include <glim_ext/dbow_loop_detector.hpp>
 #include <glim_ext/scan_context_loop_detector.hpp>
+#include <glim_ext/orb_slam_frontend.hpp>
 
 #include <glim/util/easy_profiler.hpp>
 #include <glim_ros/rviz_viewer.hpp>
@@ -40,11 +42,15 @@ public:
     const std::string config_path = ros::package::getPath("glim") + "/config";
     glim::GlobalConfig::instance(config_path);
 
+    const std::string config_ext_path = ros::package::getPath("glim_ext") + "/config";
+    glim::GlobalConfigExt::instance(config_ext_path);
+
     rviz_viewer.reset(new glim::RvizViewer);
     standard_viewer.reset(new glim::StandardViewer);
 
-    dbow_loop_detector.reset(new glim::DBoWLoopDetector);
+    // dbow_loop_detector.reset(new glim::DBoWLoopDetector);
     // sc_loop_detector.reset(new glim::ScanContextLoopDetector);
+    orb_slam_frontend.reset(new glim::OrbSLAMFrontend(true));
 
     preprocessor.reset(new glim::CloudPreprocessor);
     std::shared_ptr<glim::OdometryEstimationBase> odom(new glim::OdometryEstimation);
@@ -67,6 +73,10 @@ public:
     odometry_estimation->insert_imu(stamp, linear_acc, angular_vel);
     sub_mapping->insert_imu(stamp, linear_acc, angular_vel);
     global_mapping->insert_imu(stamp, linear_acc, angular_vel);
+  }
+
+  void insert_alphasense_imu(const double stamp, const Eigen::Vector3d& linear_acc, const Eigen::Vector3d& angular_vel) {
+    orb_slam_frontend->insert_imu(stamp, linear_acc, angular_vel);
   }
 
   void insert_frame(const glim::RawPoints::ConstPtr& raw_points) {
@@ -108,6 +118,7 @@ private:
 
   std::unique_ptr<glim::DBoWLoopDetector> dbow_loop_detector;
   std::unique_ptr<glim::ScanContextLoopDetector> sc_loop_detector;
+  std::unique_ptr<glim::OrbSLAMFrontend> orb_slam_frontend;
 
   std::unique_ptr<glim::RvizViewer> rviz_viewer;
   std::unique_ptr<glim::StandardViewer> standard_viewer;
@@ -124,34 +135,45 @@ int main(int argc, char** argv) {
     std::cout << "opening " << bag_filename << std::endl;
 
     rosbag::Bag bag(bag_filename, rosbag::bagmode::Read);
-    if(!bag.isOpen()) {
+    if (!bag.isOpen()) {
       std::cerr << glim::console::bold_red << "error: failed to open " << bag_filename << glim::console::reset << std::endl;
       return false;
     }
 
-    for(rosbag::MessageInstance const m : rosbag::View(bag, rosbag::TopicQuery(topics))) {
-      if(!glim_ros.spin_once()) {
+    for (rosbag::MessageInstance const m : rosbag::View(bag, rosbag::TopicQuery(topics))) {
+      if (!glim_ros.spin_once()) {
         return false;
       }
 
       const auto imu_msg = m.instantiate<sensor_msgs::Imu>();
-      if(imu_msg) {
+      if (imu_msg) {
         const double stamp = imu_msg->header.stamp.toSec();
         const auto& linear_acc = imu_msg->linear_acceleration;
         const auto& angular_vel = imu_msg->angular_velocity;
-        glim_ros.insert_imu(stamp, Eigen::Vector3d(linear_acc.x, linear_acc.y, linear_acc.z), Eigen::Vector3d(angular_vel.x, angular_vel.y, angular_vel.z));
+
+        if (m.getTopic().find("alphasense") != std::string::npos) {
+          glim_ros.insert_alphasense_imu(stamp, Eigen::Vector3d(linear_acc.x, linear_acc.y, linear_acc.z), Eigen::Vector3d(angular_vel.x, angular_vel.y, angular_vel.z));
+        } else {
+          glim_ros.insert_imu(stamp, Eigen::Vector3d(linear_acc.x, linear_acc.y, linear_acc.z), Eigen::Vector3d(angular_vel.x, angular_vel.y, angular_vel.z));
+        }
       }
 
       const auto points_msg = m.instantiate<sensor_msgs::PointCloud2>();
-      if(points_msg) {
+      if (points_msg) {
         auto raw_points = glim::RawPoints::extract(points_msg);
         glim_ros.insert_frame(raw_points);
       }
 
       const auto compressed_img_msg = m.instantiate<sensor_msgs::CompressedImage>();
-      if(compressed_img_msg) {
+      if (compressed_img_msg) {
         auto cv_image = cv_bridge::toCvCopy(compressed_img_msg, "bgr8");
         glim_ros.insert_image(compressed_img_msg->header.stamp.toSec(), cv_image->image);
+      }
+
+      const auto img_msg = m.instantiate<sensor_msgs::Image>();
+      if (img_msg) {
+        auto cv_image = cv_bridge::toCvCopy(img_msg, "bgr8");
+        glim_ros.insert_image(img_msg->header.stamp.toSec(), cv_image->image);
       }
 
       // Ros-related
@@ -171,22 +193,25 @@ int main(int argc, char** argv) {
   // const std::string bag_filename = "/home/koide/datasets/map_iv/data2/points.bag";
   // std::vector<std::string> topics = {"/points_raw_ex"};
 
-  const std::string bag_path = "/home/koide/datasets/newer_college/01_short_experiment/rosbag_compressed/*";
-  std::vector<std::string> topics = {"/os1_cloud_node/imu", "/os1_cloud_node/points", "/camera/infra1/image_rect_raw/compressed"};
+  // const std::string bag_path = "/home/koide/datasets/newer_college/01_short_experiment/rosbag_compressed/*";
+  // std::vector<std::string> topics = {"/os1_cloud_node/imu", "/os1_cloud_node/points", "/camera/infra1/image_rect_raw/compressed"};
+
+  const std::string bag_path = "/home/koide/datasets/hilti/uzh_tracking_area_run2.bag";
+  std::vector<std::string> topics = {"/os_cloud_node/points", "/os_cloud_node/imu", "/alphasense/cam1/image_raw", "/alphasense/imu"};
 
   std::vector<std::string> bag_filenames;
 
   glob_t globbuf;
   int ret = glob(bag_path.c_str(), 0, nullptr, &globbuf);
-  for(int i = 0; i < globbuf.gl_pathc; i++) {
+  for (int i = 0; i < globbuf.gl_pathc; i++) {
     bag_filenames.push_back(globbuf.gl_pathv[i]);
   }
   globfree(&globbuf);
 
   std::sort(bag_filenames.begin(), bag_filenames.end());
 
-  for(const auto& bag_filename : bag_filenames) {
-    if(!read_bag(bag_filename, topics)) {
+  for (const auto& bag_filename : bag_filenames) {
+    if (!read_bag(bag_filename, topics)) {
       break;
     }
   }
