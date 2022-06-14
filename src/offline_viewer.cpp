5da79557daa882f1c186f54328d7e200babf0cbe
k.koide <k.koide@aist.go.jp>
2022-06-14 10:31:46 +0900

    refactor offline viewer

--- src/offline_viewer.cpp
+++ src/offline_viewer.cpp
@@ -3,10 +3,19 @@
 #include <glim/util/config.hpp>
 #include <glim/viewer/offline_viewer.hpp>
 
+#include <gtsam/geometry/Pose3.h>
+#include <gtsam/base/serialization.h>
+
+#include <gtsam/slam/PriorFactor.h>
+#include <gtsam/nonlinear/NonlinearFactorGraph.h>
+#include <gtsam_ext/factors/linear_damping_factor.hpp>
+
 int main(int argc, char** argv) {
   const std::string config_path = ros::package::getPath("glim") + "/config";
   glim::GlobalConfig::instance(config_path);
 
   glim::OfflineViewer viewer;
   viewer.wait();
+
+  return 0;
 }
